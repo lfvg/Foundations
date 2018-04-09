77ef410f77be9e4685046d41402fb9431c23fbec
lfvg <lfvg@cin.ufpe.br>
2018-04-09 13:24:29 -0300

    Correcao de um bug
    Correcao de um bug que causava estouro do limite do array

--- teste/teste/main.swift
+++ teste/teste/main.swift
@@ -3,23 +3,31 @@ func quick(pArray:[Int])->[Int]{
     let pivo = pArray[0]
     var i = 1
     var j = arr.count-1
-    
-    while(i<j){
-        while(arr[i]>=arr[0] && i<arr.count){
-            i+=1
-        }
-        while(arr[j]<arr[0] && j>0){
-            j-=1
-        }
-        if i<=j{
-            var temp = arr[i]
-            arr[i] =  arr[j]
-            arr[j] = temp
+    if (arr.endIndex == 2){
+        if(arr[0] < arr[1]){
+            arr[0] = arr[1]
+            arr[1] = pivo
         }
     }
-    arr[0] = arr[j]
-    arr[j] = pivo
-    
+    else if(arr.endIndex>2){
+        while(i<j){
+            //erro aqui quando o array so tem um elemento
+            //esse erro foi corrigido (na verdade nao precisava do else if em cima, era so ter movido a comparacao de limimte pra antes da de valores dontro do array dos dois whiles)
+            while(i<arr.count && arr[i]>=arr[0]){
+                i+=1
+            }
+            while(j>0 && arr[j]<arr[0]){
+                j-=1
+            }
+            if i<=j{
+                var temp = arr[i]
+                arr[i] =  arr[j]
+                arr[j] = temp
+            }
+        }
+        arr[0] = arr[j]
+        arr[j] = pivo
+    }
     
     var lefti:[Int] = []
     var righti:[Int] = []
@@ -35,7 +43,7 @@ func quick(pArray:[Int])->[Int]{
     }
     if j > 0{
         //let t = arr.count
-       // print(t)
+        // print(t)
         var right = arr[j ..< arr.endIndex]
         //print("Esse é o right \(right)")
         righti = quick(pArray: Array(right))
@@ -50,5 +58,6 @@ func quick(pArray:[Int])->[Int]{
 }
 
 
-var ar = [11, 10, 30, 20]
+var ar = [11, 10, 30, 20, 54]
+print (ar.endIndex)
 print(quick(pArray: ar))
