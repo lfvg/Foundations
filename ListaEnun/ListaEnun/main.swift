836385685cb43bfd50d5edc591b66d634844f7e9
lfvg <lfvg@cin.ufpe.br>
2018-04-23 13:10:12 -0300

    Lista enum
    finalização da Lista enum

--- ListaEnun/ListaEnun/main.swift
+++ ListaEnun/ListaEnun/main.swift
@@ -5,16 +5,31 @@ enum Lista{
     //FAZER COMO EM HASKELL
      func inserir(valor:Int)->Lista{
         switch self {
-        case let .no(x, next):
-            return (x, next.inserir(valor:valor))
+        case let .no(x , next):
+            return Lista.no(x, next.inserir(valor:valor))
         case .empty:
-            var a:Lista = (valor, .empty)
+            return Lista.no(valor, .empty)
+        }
+    }
+    func imprimir (){
+        switch self {
+        case let .no(x, next):
+            print("Nó: \(x)")
+            next.imprimir()
+        default:
             return
         }
     }
-    
-    
-    
 }
 
+var teste:Lista = Lista.no(0, .empty)
 
+teste.imprimir()
+
+teste = teste.inserir(valor: 1)
+teste = teste.inserir(valor: 2)
+teste = teste.inserir(valor: 3)
+teste = teste.inserir(valor: 4)
+teste = teste.inserir(valor: 5)
+
+teste.imprimir()
