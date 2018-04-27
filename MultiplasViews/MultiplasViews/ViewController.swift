f1467a1032abc339c6ce2c166bf6969c5eeadc94
lfvg <lfvg@cin.ufpe.br>
2018-04-27 16:06:56 -0300

    Fim da aula
    Ainda falta a parte de multiplas abas

--- MultiplasViews/MultiplasViews/ViewController.swift
+++ MultiplasViews/MultiplasViews/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var meuTexto: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //esse daqui tem que linkar as storyboards arrastando da caixa em cima e dar um id para o segway(conexao)
+    @IBAction func transicaoManual(_ sender: Any) {
+        self.performSegue(withIdentifier: "meusegway", sender: self)
+    }
+    //isso faz parte da funcao de cima e pode ser usado para fazer um condicinal na hora de iniciar a procima atividade ou nao
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if self.meuTexto.text == ""{
+            return false
+        }
+        else{
+            return true
+        }
+    }
+    
+    //esse aqui tem que dar um id para o view controller no storyboard
+    @IBAction func exibeManualmente(_ sender: Any) {
+        var vc = self.storyboard?.instantiateViewController(withIdentifier: "SegundoVC")
+        self.present(vc!, animated: false, completion: nil)
+    }
+    
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue){
+        
+    }
 }
 
